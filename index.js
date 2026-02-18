9afdc02270c4d5c30ed673a47987205a07d48c80
Sohan Rout <sohanrout84@gmail.com>
2026-02-18 19:16:39 +0530

    Feat : added login and signup routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 require("./models/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -9,6 +12,10 @@ const PORT = process.env.PORT || 8080
 app.get("/ping", (req, res) => {
     res.send("Pong");
 })
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/auth", auth);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
