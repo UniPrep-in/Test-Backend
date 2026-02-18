e9bd337b7b27574bd06e79bc1279bee7ff731993
Sohan Rout <sohanrout84@gmail.com>
2026-02-18 18:54:25 +0530

    Feat : added user schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,40 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        trim: true
+    },
+    userId: {
+        type: String,
+        unique: true
+    },
+    profilePicture: {
+        type: String
+    }
+}, { timestamps: true });
+
+const UserModel = mongoose.model("Users", UserSchema);
+module.exports = UserModel;
